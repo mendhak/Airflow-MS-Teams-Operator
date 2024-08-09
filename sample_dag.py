1935e31d0a70d0c647f9632623083191a46bab11
Shah Altaf <shah.altaf@cirium.com>
2024-08-09 13:53:40 +0100

    [powerautomate] Formatting a datetime and sending it to subtitle

--- sample_dag.py
+++ sample_dag.py
@@ -13,20 +13,23 @@ from ms_teams_powerautomate_webhook_operator import MSTeamsPowerAutomateWebhookO
 def sample_dag():
 
     @task()
-    def print_something(some_value: float):
-        print(f"This task is complete. Final value, {some_value:.2f}")
+    def get_formatted_date(**kwargs):
+        iso8601date = kwargs['execution_date'].strftime("%Y-%m-%dT%H:%M:%SZ")
+        formatted_date = f"{{{{DATE({iso8601date}, SHORT)}}}} at {{{{TIME({iso8601date})}}}}"
+        print(formatted_date)
+        return formatted_date
         
+    formatted_date = get_formatted_date()
 
     op1 = MSTeamsPowerAutomateWebhookOperator(task_id='send_to_teams',
         http_conn_id='msteams_webhook_url',
         heading_message="Airflow local test",
-        heading_subtitle="This is a test message",
+        heading_subtitle=formatted_date,
         body_message="**lorem_ipsum** ran successfully in **localhost**",
         button_text="View logs",
         button_url="http://localhost:8080",
         )
 
-    print_something(123.45) >> op1
 
 
 sample_dag()
